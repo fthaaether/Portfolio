dfc28871025a4690ba587acd6f3231fb9855b5df
Muhammad Fathar Al Arsy <154654395+fthaaether@users.noreply.github.com>
2025-02-21 19:01:24 +0700

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".cur", {
+    strings: [
+        "Currently a Sigma Developer",
+        "Currently a Pro Valorant Player",
+        "Currently a Normal Student",
+        "HEHEHEHEHE",
+        "HUHUHUHUHU",
+        "HIHIHIHIHI",
+        "HEHEHEHEHE",
+        "HUHUHUHUHU",
+        "HIHIHIHIHI",
+
+    ],
+    typeSpeed: 60,
+    backSpeed: 50,
+    loop: true,
+});
